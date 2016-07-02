14562f6574423ca8e283c3bfdd5178a7734f338a
Jacob Quinn <quinn.jacobd@gmail.com>
2016-07-02 06:09:34 -0600

    Add ability to define a columns type by column name in a Dict.

--- test/runtests.jl
+++ test/runtests.jl
@@ -195,6 +195,14 @@ f = CSV.Source(joinpath(dir, "FL_insurance_sample.csv");types=Dict(10=>Float64,1
 @test f.schema.types == [Int,WeakRefString{UInt8},WeakRefString{UInt8},Float64,Float64,Float64,Float64,Float64,Float64,Float64,Float64,Float64,Int,Float64,Float64,WeakRefString{UInt8},WeakRefString{UInt8},Int]
 ds = Data.stream!(f, DataFrame)
 
+f = CSV.Source(joinpath(dir, "FL_insurance_sample.csv");types=Dict("eq_site_deductible"=>Float64,"fl_site_deductible"=>Float64))
+@test f.schema.cols == 18
+@test f.schema.rows == 36634
+@test position(f.data) == 243
+@test f.schema.header == ["policyID","statecode","county","eq_site_limit","hu_site_limit","fl_site_limit","fr_site_limit","tiv_2011","tiv_2012","eq_site_deductible","hu_site_deductible","fl_site_deductible","fr_site_deductible","point_latitude","point_longitude","line","construction","point_granularity"]
+@test f.schema.types == [Int,WeakRefString{UInt8},WeakRefString{UInt8},Float64,Float64,Float64,Float64,Float64,Float64,Float64,Float64,Float64,Int,Float64,Float64,WeakRefString{UInt8},WeakRefString{UInt8},Int]
+ds = Data.stream!(f, DataFrame)
+
 f = CSV.Source(joinpath(dir, "SacramentocrimeJanuary2006.csv"))
 @test f.schema.cols == 9
 @test f.schema.rows == 7584
