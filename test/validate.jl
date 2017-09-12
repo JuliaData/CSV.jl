b866c7d18d40967d6737e5504d1c0a8a962a80a8
quinnj <quinn.jacobd@gmail.com>
2017-09-12 10:13:51 -0600

    Cleanup validate.jl tests

--- test/validate.jl
+++ test/validate.jl
@@ -13,3 +13,9 @@ io = IOBuffer("""A;B;C
 1,1,10
 2,0,16""")
 @test_throws CSV.ExpectedMoreColumnsError CSV.validate(io; delim=';')
+
+io = IOBuffer("""a b c d e
+1 2  3 4 5
+1 2 3  4 5
+1  2 3  4 5""")
+@test_throws CSV.TooManyColumnsError CSV.validate(io; delim=' ')
