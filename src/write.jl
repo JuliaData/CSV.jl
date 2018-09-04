9813fadc954e3a6cdf7c52baf7de34b9d0c1af7a
quinnj <quinn.jacobd@gmail.com>
2018-09-04 08:26:14 -0600

    Updates

--- src/write.jl
+++ src/write.jl
@@ -74,7 +74,50 @@ function printcsv(io, val::T, delim, oq, cq, e, df) where {T <: Dates.TimeType}
 end
 
 write(file::Union{String, IO}; kwargs...) = x->write(x, file; kwargs...)
-function write(itr, file::Union{String, IO};
+function write(itr, file::Union{String, IO}; kwargs...)
+    rows = Tables.rows(itr)
+    sch = Tables.schema(rows)
+    return write(sch, rows, file; kwargs...)
+end
+
+function printheader(io, header, delim, oq, cq, e, df)
+    cols = length(header)
+    for (col, nm) in enumerate(header)
+        printcsv(io, string(nm), delim, oq, cq, d, df)
+        Base.write(io, ifelse(col == cols, UInt8('\n'), delim))
+    end
+    return
+end
+
+function write(sch::Tables.Schema{schema_names}, rows, file::Union{String, IO};
+    delim::Union{Char, String}=',',
+    quotechar::Char='"',
+    openquotechar::Union{Char, Nothing}=nothing,
+    closequotechar::Union{Char, Nothing}=nothing,
+    escapechar::Char='\\',
+    missingstring::AbstractString="",
+    dateformat=nothing,
+    append::Bool=false,
+    writeheader::Bool=!append,
+    header::Vector=String[],
+    ) where {schema_names}
+    oq, cq = openquotechar !== nothing ? (openquotechar, closequotechar) : (quotechar, quotechar)
+    names = isempty(header) ? schema_names : header
+    cols = length(names)
+    with(file, append) do io
+        writeheader && printheader(io, names, delim, oq, cq, escapechar, dateformat)
+        for row in rows
+            Tables.eachcolumn(sch, row) do val, col, nm
+                printcsv(io, coalesce(val, missingstring), delim, oq, cq, escapechar, dateformat)
+                Base.write(io, ifelse(col == cols, UInt8('\n'), delim))
+            end
+        end
+    end
+    return file
+end
+
+# handle unknown schema case
+function write(::Nothing, rows, file::Union{String, IO};
     delim::Union{Char, String}=',',
     quotechar::Char='"',
     openquotechar::Union{Char, Nothing}=nothing,
@@ -87,22 +130,28 @@ function write(itr, file::Union{String, IO};
     header::Vector=String[],
     )
     oq, cq = openquotechar !== nothing ? (openquotechar, closequotechar) : (quotechar, quotechar)
-    rows = Tables.rows(itr)
-    sch = Tables.schema(rows)
-    names = isempty(header) ? sch.names : header
-    cols = length(names)
-    with(file, append) do io
-        if writeheader
-            for (col, nm) in enumerate(names)
-                printcsv(io, string(nm), delim, oq, cq, escapechar, dateformat)
-                Base.write(io, ifelse(col == cols, UInt8('\n'), delim))
+    state = iterate(rows)
+    if state === nothing
+        if writeheader && !isempty(header)
+            with(file, append) do io
+                printheader(io, header, delim, oq, cq, escapechar, dateformat)
             end
         end
-        for row in rows
-            Tables.eachcolumn(sch, row) do val, col, nm
+        return file
+    end
+    row, st = state
+    names = isempty(header) ? propertynames(row) : header
+    cols = length(names)
+    with(file, append) do io
+        writeheader && printheader(io, names, delim, oq, cq, escapechar, dateformat)
+        while true
+            Tables.eachcolumn(names, row) do val, col, nm
                 printcsv(io, coalesce(val, missingstring), delim, oq, cq, escapechar, dateformat)
                 Base.write(io, ifelse(col == cols, UInt8('\n'), delim))
             end
+            state = iterate(rows, st)
+            state === nothing && break
+            row, st = state
         end
     end
     return file
