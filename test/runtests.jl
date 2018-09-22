6126d7cc24cef1a1fd0d712766a032116c57160b
quinnj <quinn.jacobd@gmail.com>
2018-09-22 08:16:29 -0600

    Add tests for transform shim

--- test/runtests.jl
+++ test/runtests.jl
@@ -40,6 +40,51 @@ include("write.jl")
     @test_throws CSV.TooManyColumnsError CSV.validate(io; delim=' ')
 end
 
+@testset "transform" begin
+    csv = """A,B,C
+    1,1.0,"hey"
+    ,2.0,"there"
+    3,3.0,"sailor"
+    """
+
+    df = CSV.read(IOBuffer(csv); transforms=Dict("A"=>x->x+1))
+    @test isequal(df.A, [2, missing, 4])
+    @test typeof(df.A) == Vector{Union{Missing, Int64}}
+    @test size(df) == (3, 3)
+
+    df = CSV.read(IOBuffer(csv); transforms=Dict("A"=>x->coalesce(x+1, 0)))
+    @test df.A == [2, 0, 4]
+    @test typeof(df.A) == Vector{Int64}
+
+    df = CSV.read(IOBuffer(csv); transforms=Dict("A"=>x->coalesce(x+1, 0.0)))
+    @test df.A == [2, 0.0, 4]
+    @test typeof(df.A) == Vector{Real}
+
+    df = CSV.read(IOBuffer(csv); transforms=Dict(2=>x->x==2.0 ? missing : x))
+    @test size(df) == (3, 3)
+    @test isequal(df.B, [1.0, missing, 3.0])
+    @test typeof(df.B) == Vector{Union{Float64, Missing}}
+
+    f = CSV.File(IOBuffer(csv); allowmissing=:auto)
+
+    # transforms on CSV.Columns
+    df = f |> transform(A=x->x+1) |> DataFrame
+    @test size(df) == (3, 3)
+    df = f |> transform(A=x->x+1) |> columntable
+    @test length(df) == 3
+    df = f |> transform(A=x->x+1) |> rowtable
+    @test length(df) == 3
+
+    # transforms on CSV.RowIterator
+    f = CSV.File(joinpath(dir, "pandas_zeros.csv"), allowmissing=:none);
+    df = f |> transform(_0=x->x+1) |> DataFrame;
+    @test size(df) == (100000, 50)
+    df = f |> transform(_0=x->x+1) |> columntable;
+    @test length(df) == 50
+    df = f |> transform(_0=x->x+1) |> rowtable;
+    @test length(df) == 100000
+end
+
 include("deprecated.jl")
 
 end
