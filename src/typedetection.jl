1cbf7acc23fc3afe5d6a62e149b1e3ee1a9e29ff
Jacob Quinn <quinn.jacobd@gmail.com>
2018-10-01 21:11:22 -0600

    Fix #310 by making sure we don't throw an error when trying to parse … (#312)
    * Fix #310 by making sure we don't throw an error when trying to parse datetimes

--- src/typedetection.jl
+++ src/typedetection.jl
@@ -1,36 +1,23 @@
 # initialization case
-promote_type2(::Type{Union{}}, ::Type{T}) where {T} = T
-promote_type2(::Type{Union{}}, ::Type{String}) = String
-promote_type2(::Type{Missing}, ::Type{T}) where {T} = Union{T, Missing}
-promote_type2(::Type{Missing}, ::Type{String}) = Union{String, Missing}
-promote_type2(::Type{Int64}, ::Type{Missing}) = Union{Int64, Missing}
-promote_type2(::Type{Float64}, ::Type{Missing}) = Union{Float64, Missing}
-promote_type2(::Type{Bool}, ::Type{Missing}) = Union{Bool, Missing}
-promote_type2(::Type{Date}, ::Type{Missing}) = Union{Date, Missing}
-promote_type2(::Type{DateTime}, ::Type{Missing}) = Union{DateTime, Missing}
-promote_type2(::Type{String}, ::Type{Missing}) = Union{String, Missing}
-# basic promote type definitions from Base
-promote_type2(::Type{Int64}, ::Type{Float64}) = Float64
-promote_type2(::Type{Float64}, ::Type{Int64}) = Float64
-promote_type2(::Type{Int64}, ::Type{Date}) = String
-promote_type2(::Type{Int64}, ::Type{DateTime}) = String
-promote_type2(::Type{Date}, ::Type{Int64}) = String
-promote_type2(::Type{DateTime}, ::Type{Int64}) = String
-promote_type2(::Type{Float64}, ::Type{Date}) = String
-promote_type2(::Type{Float64}, ::Type{DateTime}) = String
-promote_type2(::Type{Date}, ::Type{Float64}) = String
-promote_type2(::Type{DateTime}, ::Type{Float64}) = String
-promote_type2(::Type{Date}, ::Type{DateTime}) = DateTime
-promote_type2(::Type{DateTime}, ::Type{Date}) = DateTime
-# for cases when our current type can't widen, just promote to String
-promote_type2(::Type{T}, ::Type{String}) where {T} = String
-promote_type2(::Type{String}, ::Type{T}) where {T} = String
-promote_type2(::Type{Union{T, Missing}}, ::Type{String}) where {T} = Union{String, Missing}
-promote_type2(::Type{Union{String, Missing}}, ::Type{String}) = Union{String, Missing}
-# this definitions allow Union{Int64, Missing} to promote to Union{Float64, Missing}
-promote_type2(::Type{Union{T, Missing}}, ::Type{S}) where {T, S} = Union{promote_type2(T, S), Missing}
-promote_type2(::Type{T}, ::Type{T}) where {T} = T
-promote_type2(::Type{String}, ::Type{String}) = String
+function promote_type2(@nospecialize(T), @nospecialize(S))
+    if T === Union{} || T === S
+        return S
+    elseif T === Missing || S === Missing
+        return Union{T, S}
+    elseif T === Int64
+        return S === Float64 ? Float64 : String
+    elseif T === Float64
+        return S === Int64 ? Float64 : String
+    elseif T === Date
+        return S === DateTime ? DateTime : String
+    elseif T === DateTime
+        return S === Date ? DateTime : String
+    elseif T === String
+        return String
+    else
+        return S === String ? Union{String, Missing} : Union{promote_type2(Base.nonmissingtype(T), Base.nonmissingtype(S)), Missing}
+    end
+end
 
 # providedtypes: Dict{String, Type}, Dict{Int, Type}, Vector{Type}
 initialtype(allowmissing) = (allowmissing === :auto || allowmissing === :none) ? Union{} : Missing
@@ -62,9 +49,8 @@ function detect(types, io, positions, parsinglayers, kwargs, typemap, categorica
     len = transpose ? ref[] : length(positions)
     cols = length(types)
 
-    defaultstringtype = String
     # prep if categorical
-    levels = categorical ? Any[Dict{String, Int}() for _ = 1:cols] : []
+    levels = categorical ? Dict{String, Int}[Dict{String, Int}() for _ = 1:cols] : Dict{String, Int}[]
 
     lastcode = Base.RefValue(Parsers.SUCCESS)
     rows = 0
@@ -78,14 +64,14 @@ function detect(types, io, positions, parsinglayers, kwargs, typemap, categorica
                 continue
             end
             transpose && seek(io, positions[col])
-            if debug
-                pos = position(io)
-                debug && println("pos=$pos")
-                result = Parsers.parse(parsinglayers, io, String)
-                seek(io, pos)
-                debug && println("col: $col, detecting type for: '$(result.result)'")
-            end
-            T = detecttype(types[col], io, parsinglayers, kwargs, levels, row, col, categorical, defaultstringtype, lastcode)
+            # if debug
+            #     pos = position(io)
+            #     debug && println("pos=$pos")
+            #     result = Parsers.parse(parsinglayers, io, String)
+            #     seek(io, pos)
+            #     debug && println("col: $col, detecting type for: '$(result.result)'")
+            # end
+            T = detecttype(Base.nonmissingtype(types[col]), io, parsinglayers, kwargs, levels, row, col, categorical, lastcode)
             debug && println("col: $col, detected type: $T")
             types[col] = promote_type2(types[col], T)
             debug && println("col: $col, promoted to: $(types[col])")
@@ -115,13 +101,14 @@ function detect(types, io, positions, parsinglayers, kwargs, typemap, categorica
     return types, pools
 end
 
-empty(::Type{Union{}}) = true
-empty(::Type{Missing}) = true
-empty(x) = false
-strtype(::Type{Missing}, T) = Missing
-strtype(x, T) = T
+@inline function trytype(io, pos, layers, T, kwargs, lastcode)
+    seek(io, pos)
+    res = Parsers.parse(layers, io, T; kwargs...)
+    lastcode[] = res.code
+    return Parsers.ok(res.code) ? typeof(res.result) : nothing
+end
 
-function detecttype(prevT, io, layers, kwargs, levels, row, col, categorical, defaultstringtype, lastcode)
+function detecttype(@nospecialize(prevT), io, layers, kwargs, levels, row, col, categorical, lastcode)
     pos = position(io)
     if categorical
         result = Parsers.parse(layers, io, Tuple{Ptr{UInt8}, Int})
@@ -132,56 +119,36 @@ function detecttype(prevT, io, layers, kwargs, levels, row, col, categorical, de
         # update levels
         incr!(levels[col], result.result::Tuple{Ptr{UInt8}, Int})
     end
-
-    if Int64 <: prevT || empty(prevT)
-        seek(io, pos)
-        res_int = Parsers.parse(layers, io, Int64)
-        lastcode[] = res_int.code
-        # @debug "res_int = $res_int"
-        Parsers.ok(res_int.code) && return typeof(res_int.result)
+    if prevT === Union{} || prevT === Missing || prevT === Int64
+        int = trytype(io, pos, layers, Int64, kwargs, lastcode)
+        int !== nothing && return int
     end
-    if Float64 <: prevT || Int64 <: prevT || empty(prevT)
-        seek(io, pos)
-        res_float = Parsers.parse(layers, io, Float64; kwargs...)
-        lastcode[] = res_float.code
-        # @debug "res_float = $res_float"
-        Parsers.ok(res_float.code) && return typeof(res_float.result)
+    if prevT === Union{} || prevT === Missing || prevT === Int64 || prevT === Float64
+        float = trytype(io, pos, layers, Float64, kwargs, lastcode)
+        float !== nothing && return float
     end
-    if Date <: prevT || DateTime <: prevT || empty(prevT)
+    if prevT === Union{} || prevT === Missing || prevT === Date || prevT === DateTime
         if !haskey(kwargs, :dateformat)
-            # try to auto-detect TimeType
-            seek(io, pos)
-            res_date = Parsers.parse(layers, io, Date)
-            lastcode[] = res_date.code
-            # @debug "res_date = $res_date"
-            Parsers.ok(res_date.code) && return typeof(res_date.result)
-            seek(io, pos)
-            res_datetime = Parsers.parse(layers, io, DateTime)
-            lastcode[] = res_datetime.code
-            # @debug "res_datetime = $res_datetime"
-            Parsers.ok(res_datetime.code) && return typeof(res_datetime.result)
+            date = trytype(io, pos, layers, Date, kwargs, lastcode)
+            date !== nothing && return date
+            try
+                datetime = trytype(io, pos, layers, DateTime, kwargs, lastcode)
+                datetime !== nothing && return datetime
+            catch e
+            end
         else
             # use user-provided dateformat
             T = timetype(kwargs.dateformat)
-            # @debug "T = $T"
-            seek(io, pos)
-            res_dt = Parsers.parse(layers, io, T; dateformat=kwargs.dateformat)
-            lastcode[] = res_dt.code
-            # @debug "res_dt = $res_dt"
-            Parsers.ok(res_dt.code) && return typeof(res_dt.result)
+            dt = trytype(io, pos, layers, T, kwargs, lastcode)
+            dt !== nothing && return dt
         end
     end
-    if Bool <: prevT || empty(prevT)
-        seek(io, pos)
-        res_bool = Parsers.parse(layers, io, Bool; kwargs...)
-        lastcode[] = res_bool.code
-        # @debug "res_bool = $res_bool"
-        Parsers.ok(res_bool.code) && return typeof(res_bool.result)
+    if prevT === Union{} || prevT === Missing || prevT === Bool
+        bool = trytype(io, pos, layers, Bool, kwargs, lastcode)
+        bool !== nothing && return bool
     end
-    seek(io, pos)
-    res_str = Parsers.parse(layers, io, Tuple{Ptr{UInt8}, Int})
-    lastcode[] = res_str.code
-    return strtype(typeof(res_str.result), defaultstringtype)
+    str = trytype(io, pos, layers, Tuple{Ptr{UInt8}, Int}, kwargs, lastcode)
+    return ifelse(str === nothing, String, ifelse(str === Missing, Missing, String))
 end
 
 function timetype(df::Dates.DateFormat)
