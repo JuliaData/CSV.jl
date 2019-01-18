85bc5ad4fefa42c49d2942b09d6fa1b1acb7889b
Milan Bouchet-Valat <nalimilan@club.fr>
2019-01-18 16:49:52 +0100

    Fix sorting of categorical levels
    When all levels have been found during type detection, they were not sorted
    since sorting was only performed when added a new level during iteration.

--- test/runtests.jl
+++ test/runtests.jl
@@ -105,6 +105,20 @@ end
     v = iterate(f, 4)[1].X
     @test v == "c"
     @test levels(v.pool) == ["a", "b", "c"]
+
+    f = CSV.File(IOBuffer("X\nb\nc\na\nc"), categorical=true)
+    v = iterate(f, 1)[1].X
+    @test v == "b"
+    @test levels(v.pool) == ["a", "b", "c"]
+    v = iterate(f, 2)[1].X
+    @test v == "c"
+    @test levels(v.pool) == ["a", "b", "c"]
+    v = iterate(f, 3)[1].X
+    @test v == "a"
+    @test levels(v.pool) == ["a", "b", "c"]
+    v = iterate(f, 4)[1].X
+    @test v == "c"
+    @test levels(v.pool) == ["a", "b", "c"]
 end
 
 include("deprecated.jl")
