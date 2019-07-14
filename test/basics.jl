2bde38b5fbea7da8633c3976963ec28404134cf1
Aaron Silberstein <aaronmichaelsilberstein@gmail.com>
2019-07-13 20:59:25 -0500

    Better testing support for Windows

--- test/basics.jl
+++ test/basics.jl
@@ -340,6 +340,6 @@ df = CSV.read(IOBuffer("a,b,c\n1,2,3\n\n"), ignoreemptylines=true)
 df = CSV.read(IOBuffer("zip\n11111-1111\n"), dateformat = "y-m-dTH:M:S.s")
 @test size(df) == (1, 1)
 
-@test CSV.read(`$(Base.Sys.iswindows() ? "type" : "cat") $(joinpath(dir, "test_basic.csv"))`) == CSV.read(joinpath(dir, "test_basic.csv"))
+@test CSV.read(`$(Base.Sys.isunix() ? "cat" : "type") $(joinpath(dir, "test_basic.csv"))`) == CSV.read(joinpath(dir, "test_basic.csv"))
 
 end
