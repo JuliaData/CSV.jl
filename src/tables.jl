e8a2afbedcd4613a70ab42612fcfae52930f1c34
Jacob Quinn <quinn.jacobd@gmail.com>
2019-12-12 10:51:44 -0700

    Fixup copy for Column2; remove some duplicated code; add some fast-paths

--- src/tables.jl
+++ src/tables.jl
@@ -49,102 +49,52 @@ Base.@propagate_inbounds function Base.getindex(c::Column2, i::Integer)
     throw(BoundsError(c, i))
 end
 
-function Base.copy(c::Column2{T}) where {T}
+function Base.copy(c::Union{Column{T}, Column2{T}}) where {T}
     len = length(c)
-    A = Vector{T}(undef, len)
+    A = (T == String || T == Union{String, Missing}) ? StringVector{T}(undef, len) : Vector{T}(undef, len)
+    if T <: Int64 || T <: Float64 || T <: Dates.TimeType
+        if c isa Column
+            memcpy!(pointer(A), 1, pointer(c.tape), 1, len * 8)
+            return A
+        else
+            doff = 1
+            for cx in c.columns
+                n = length(cx.tape) * 8
+                memcpy!(pointer(A), doff, pointer(cx.tape), 1, n)
+                doff += n
+            end
+            return A
+        end
+    end
     for (i, x) in enumerate(c)
         @inbounds A[i] = x
     end
     return A
 end
 
-function Base.copy(c::Column{T}) where {T}
-    len = length(c)
-    A = Vector{T}(undef, len)
-    @simd for i = 1:len
-        @inbounds A[i] = c[i]
-    end
-    return A
-end
-
-function Base.copy(c::Column2{T, T}) where {T <: Union{String, Union{String, Missing}}}
-    len = length(c)
-    A = StringVector{T}(undef, len)
-    for (i, x) in enumerate(c)
-        @inbounds A[i] = x
-    end
-    return A
-end
-
-function Base.copy(c::Column{T, T}) where {T <: Union{String, Union{String, Missing}}}
-    len = length(c)
-    A = StringVector{T}(undef, len)
-    @simd for i = 1:len
-        @inbounds A[i] = c[i]
-    end
-    return A
-end
-
-function Base.copy(c::Column2{T, S}) where {T <: Union{String, Union{String, Missing}}, S <: Union{PooledString, Union{PooledString, Missing}}}
-    len = length(c)
-    c1 = c.columns[1]
-    catg = c1.catg
-    crefs = c1.refs
-    if S === PooledString
-        refs = Dict{String, UInt32}()
-        foreach(x->setindex!(refs, UInt32(x[1]), x[2]), enumerate(crefs))
-        values = Vector{UInt32}(undef, len)
-        j = 1
-        for cx in c.columns
-            tape = cx.tape
-            @simd for i = 1:length(tape)
-                @inbounds values[j] = ref(tape[i])
-                j += 1
-            end
-        end
-    else
-        if catg
-            refs = Dict{String, UInt32}()
-            foreach(x->setindex!(refs, UInt32(x[1]), x[2]), enumerate(crefs))
-            missingref = UInt32(0)
-            values = Vector{UInt32}(undef, len)
-        else # Union{PooledString, Missing}
-            refs = Dict{Union{String, Missing}, UInt32}()
-            foreach(x->setindex!(refs, UInt32(x[1]), x[2]), enumerate(crefs))
-            missingref = UInt32(length(refs) + 1)
-            refs[missing] = missingref
-            values = Vector{UInt32}(undef, len)
-        end
-        for cx in c.columns
-            tape = cx.tape
-            @simd for i = 1:length(tape)
-                @inbounds v = ref(tape[i])
-                @inbounds values[j] = ifelse(v == UInt32(0), missingref, v)
-                j += 1
-            end
-        end
-    end
-    if catg
-        pool = CategoricalPool(refs)
-        levels!(pool, sort(levels(pool)))
-        A = CategoricalArray{T, 1}(values, pool)
-    else
-        A = PooledArray(PooledArrays.RefArray(values), refs)
-    end
-    return A
-end
-
-function Base.copy(c::Column{T, S}) where {T <: Union{String, Union{String, Missing}}, S <: Union{PooledString, Union{PooledString, Missing}}}
-    len = length(c)
+function Base.copy(col::Union{Column{T, S}, Column2{T, S}}) where {T <: Union{String, Union{String, Missing}}, S <: Union{PooledString, Union{PooledString, Missing}}}
+    len = length(col)
+    c = col isa Column ? col : c.columns[1]
     catg = c.catg
-    tape = c.tape
     crefs = c.refs
     if S === PooledString
         refs = Dict{String, UInt32}()
         foreach(x->setindex!(refs, UInt32(x[1]), x[2]), enumerate(crefs))
         values = Vector{UInt32}(undef, len)
-        @simd for i = 1:len
-            @inbounds values[i] = ref(tape[i])
+        if col isa Column
+            tape = c.tape
+            @simd for i = 1:len
+                @inbounds values[i] = ref(tape[i])
+            end
+        else
+            i = 1
+            for cx in col.columns
+                tape = cx.tape
+                @simd for j = 1:length(cx)
+                    @inbounds values[i] = ref(tape[j])
+                    i += 1
+                end
+            end
         end
     else
         if catg
@@ -159,9 +109,22 @@ function Base.copy(c::Column{T, S}) where {T <: Union{String, Union{String, Miss
             refs[missing] = missingref
             values = Vector{UInt32}(undef, len)
         end
-        @simd for i = 1:len
-            @inbounds v = ref(tape[i])
-            @inbounds values[i] = ifelse(v == UInt32(0), missingref, v)
+        if col isa Column
+            tape = c.tape
+            @simd for i = 1:len
+                @inbounds v = ref(tape[i])
+                @inbounds values[i] = ifelse(v == UInt32(0), missingref, v)
+            end
+        else
+            i = 1
+            for cx in col.columns
+                tape = cx.tape
+                @simd for j = 1:length(cx)
+                    @inbounds v = ref(tape[i])
+                    @inbounds values[i] = ifelse(v == UInt32(0), missingref, v)
+                    i += 1
+                end
+            end
         end
     end
     if catg
