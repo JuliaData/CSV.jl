e8e81f851d6cb9b915bbb642efe4666b9236a433
Ian Butterworth <i.r.butterworth@gmail.com>
2020-08-11 00:29:45 -0400

    Add canonical url to docs generator (#717)
    * add canonical url to docs generator
    
    * Update docs/make.jl
    
    Co-authored-by: Morten Piibeleht <morten.piibeleht@gmail.com>
    
    Co-authored-by: Morten Piibeleht <morten.piibeleht@gmail.com>

--- docs/make.jl
+++ docs/make.jl
@@ -1,5 +1,5 @@
 using Documenter, CSV
 
-makedocs(modules = [CSV], sitename = "CSV.jl")
+makedocs(modules = [CSV], sitename = "CSV.jl", format = Documenter.HTML(canonical = "https://juliadata.github.io/CSV.jl/stable/"))
 
 deploydocs(repo = "github.com/JuliaData/CSV.jl.git")
