9a7fa3613aefc4e594fb8f878d2991b28751edcc
Jacob Quinn <quinn.jacobd@gmail.com>
2021-10-20 20:38:45 -0600

    Fix corner case when string promotion produces diff type than promoted
    Fixes #932. The core issue here is the scenario where the original
    multithreaded parse tasks all parse as a non-string (`Int64`) or string
    type (like `String3`), but when the non-string tasks go to re-parse
    their chunks as string type, it produces a "wider" string type than was
    originally promoted. For example, given the types mentioned above, after
    intial parsing, the promoted column type would be `String3`, but when a
    task goes to reparse the column, it may detect the column type needs to
    be wider like `String7`. The problem is that the multithreaded column
    post-processing code assumed the originally promoted column type was
    "final" and hence bad things happened when a threaded column type was
    wider than the promoted type, which shouldn't be possible.
    
    The fix then is to still promote the column to string, but we need to do
    that across all threads first, re-promoting the final column type, and
    then proceed with the other column post-processing tasks like ref
    syncing, etc.

--- src/detection.jl
+++ src/detection.jl
@@ -388,6 +388,7 @@ function findchunkrowstart(ranges, i, buf, opts, downcast, ncols, rows_to_check,
                 end
                 pos += tlen
                 if Parsers.newline(code)
+                    parsedncols += 1
                     rowsparsed += 1
                     break
                 end
