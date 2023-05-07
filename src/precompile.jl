ec6e0c1ba18b8f6f413ddd88c897baf30c7487e1
Tim Holy <tim.holy@gmail.com>
2023-05-07 15:15:33 -0500

    SnoopPrecompile -> PrecompileTools

--- src/precompile.jl
+++ src/precompile.jl
@@ -1,10 +1,10 @@
-using SnoopPrecompile
+using PrecompileTools
 
-@precompile_setup begin
+@setup_workload begin
     # Putting some things in `setup` can reduce the size of the
     # precompile file and potentially make loading faster.
     PRECOMPILE_DATA = "int,float,date,datetime,bool,null,str,catg,int_float\n1,3.14,2019-01-01,2019-01-01T01:02:03,true,,hey,abc,2\n2,NaN,2019-01-02,2019-01-03T01:02:03,false,,there,abc,3.14\n"
-    @precompile_all_calls begin
+    @compile_workload begin
         # all calls in this block will be precompiled, regardless of whether
         # they belong to your package or not (on Julia 1.8 and higher)
         CSV.File(IOBuffer(PRECOMPILE_DATA))
